062fadf038cdc085534b307a148dce7fa7100afb
Darren Royle <darren@diffblue.com>
2018-04-13 14:39:57 +0100

    Firebasing

---
+++ webpack.production.config.js
@@ -0,0 +1,36 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'public'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?modules',
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin()
+  ],
+  mode: 'production'
+};
